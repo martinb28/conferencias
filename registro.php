93c90da8dcb33b00db0beb368e21896397e76b12
Martin <bugao1991@gmail.com>
2022-07-09 17:02:10 -0300

    practica con post y precarga del formulario

---
+++ registro.php
@@ -0,0 +1,20 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$mail = $_POST['mail'];
+$clave = $_POST['clave'];
+$edad = $_POST['edad'];
+
+echo '<br>';
+echo $nombre;
+echo '<br>';
+echo $apellido;
+echo '<br>';
+echo $mail;
+echo '<br>';
+echo $clave;
+echo '<br>';
+echo $edad;
+
+?>
